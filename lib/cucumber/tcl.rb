799399d1fdfdffa65b2180874d408fe60cbb2137
Matt Wynne <matt@mattwynne.net>
2015-02-06 16:17:16 +0000

    Add failing test for Given/When/Then framework
    Over to you @jowers ;)

--- lib/cucumber/tcl.rb
+++ lib/cucumber/tcl.rb
@@ -3,7 +3,7 @@ require 'cucumber/tcl/step_definitions'
 
 if respond_to?(:AfterConfiguration)
   AfterConfiguration do |config|
-    step_definitions = Cucumber::Tcl::StepDefinitions.new('features/support/env.tcl')
+    step_definitions = Cucumber::Tcl::StepDefinitions.new(File.dirname(__FILE__) + '/tcl/framework.tcl')
     config.filters << Cucumber::Tcl::ActivateSteps.new(step_definitions)
   end
 end
