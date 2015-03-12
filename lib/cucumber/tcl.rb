8acdae21bf8dc21ea07469d36b56544f592f71c9
Matt Wynne <matt@mattwynne.net>
2015-03-12 10:41:54 +0000

    Refactor, extracting install method

--- lib/cucumber/tcl.rb
+++ lib/cucumber/tcl.rb
@@ -1,11 +1,20 @@
 require 'cucumber/tcl/activate_steps'
 require 'cucumber/tcl/step_definitions'
 
+# Use Cucumber's configuration hook to install the plugin
 if respond_to?(:AfterConfiguration)
   AfterConfiguration do |config|
-    create_step_definitions = lambda {
-      Cucumber::Tcl::StepDefinitions.new(File.dirname(__FILE__) + '/tcl/framework.tcl')
-    }
-    config.filters << Cucumber::Tcl::ActivateSteps.new(create_step_definitions)
+    Cucumber::Tcl.install config
+  end
+end
+
+module Cucumber
+  module Tcl
+    def self.install(cucumber_config)
+      create_step_definitions = lambda {
+        StepDefinitions.new(File.dirname(__FILE__) + '/tcl/framework.tcl')
+      }
+      cucumber_config.filters << ActivateSteps.new(create_step_definitions)
+    end
   end
 end
